feca5cfe08d95d6f07403cb3c2d4f66aa5757a7b
Jayaprakash Reddy Peddagangannagari <JayaprakashReddy@users.noreply.github.com>
2017-05-15 12:30:13 +1000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+   i <- NULL
+   set <- function(y) {
+    m <<- y
+    i <<- NULL
+   }
 
+   get <- function() m
+   setSolve <- function(solve) {
+    i <<- solve
+   }
+   getSolve <- function() i
+   list( set=set, get=get, setSolve= setSolve, getSolve=getSolve )
 }
 
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getSolve()
+  if(!is.null(i)) {
+        message("getting cv1ached data")
+	return(i)
+  }
+  data <- x$get()
+  i <- solve(data)
+  x$setSolve(i)
+  i        
 }
